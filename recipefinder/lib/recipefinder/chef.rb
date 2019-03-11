d59934e79156c53fa2e235c421bf722e80efeb18
NYHolzer <nissan.holzer@gmail.com>
2019-03-10 22:05:01 -0400

    adding instance variables to classes

--- recipefinder/lib/recipefinder/chef.rb
+++ recipefinder/lib/recipefinder/chef.rb
@@ -1,3 +1,11 @@
 class Chef
   attr_accessor :name, :recipes
+
+  def recipes
+    @recipes = []
+    Recipe.all.each do |recipe|
+      recipe.chef == self.name
+      self.recipes << recipe
+    end
+  end
 end
--- recipefinder/lib/recipefinder/recipe.rb
+++ recipefinder/lib/recipefinder/recipe.rb
@@ -1,3 +1,14 @@
 class Recipe
+  #all will get from the scrapper
   attr_accessor :name, :description, :ingredients, :directions, :chef, :link
+
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
+  def chef=(chef)
+    @chef = Chef.new(chef)
+  end
 end
