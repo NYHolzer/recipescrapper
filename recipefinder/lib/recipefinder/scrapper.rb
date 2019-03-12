61bcdeeca431b8f7e3bceb2b1ac7bd87a7041d9c
NYHolzer <nissan.holzer@gmail.com>
2019-03-11 20:33:38 -0400

    created Scrapper class. #site, #get_page, #get_recipes, #make_recipes (start)

--- recipefinder/lib/recipefinder/scrapper.rb
+++ recipefinder/lib/recipefinder/scrapper.rb
@@ -2,20 +2,46 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
+require_relative './chef.rb'
+require_relative './recipe.rb'
+require_relative './course.rb'
+
 site = "https://www.allrecipes.com/recipes/79/desserts/?page=1"
 site2 = "https://www.allrecipes.com/recipe/10702/jam-thumbprints/"
 
 doc = Nokogiri::HTML(open(site))
 doc2 = Nokogiri::HTML(open(site2))
 
-binding.pry
+class Scrapper
 
+  def site   #Leaving option open to change site based on user input
+    @site = "https://www.allrecipes.com/recipes/79/desserts/"
+  end
+
+  def get_page
+    Nokogiri::HTML(open(self.site))
+  end
+
+  def get_recipes
+    self.get_page.css("article.fixed-recipe-card")
+  end
+
+  def make_recipes
+    self.get_recipes.count
+    binding.pry
+  end
+
+end
+
+Scrapper.new.make_recipes
 #:title = doc.css("article.fixed-recipe-card")[0].css(".fixed-recipe-card__title-link")[1].text
 #:description = doc.css("article.fixed-recipe-card")[0].css(".fixed-recipe-card__description").text
 #:link =  doc.css("article.fixed-recipe-card")[0].css("a")[0].values[0]
 #:chef = doc.css("article.fixed-recipe-card")[0].css(".cook-submitter-info h4").text.delete("By").lstrip
 
-------
+
+
+#------
 #2nd site within each recipe lookup:
 #:ingredients = doc2.css("span.recipe-ingred_txt.added")[7].text -- count the array and then loop through the array for .text - Filter out "Add all ingredients to list"
 #:directions = doc2.css("span.recipe-directions__list--item")[3].text -- count the array and then loop through - filter out empty ""
