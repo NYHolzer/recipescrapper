a0d8307d8d2838718b4eae7605767a92a864f702
NYHolzer <nissan.holzer@gmail.com>
2019-03-14 22:46:41 -0400

    Added all 3 options appetizer, main, dessert.

--- recipefinder/lib/recipefinder/cli.rb
+++ recipefinder/lib/recipefinder/cli.rb
@@ -12,6 +12,7 @@ class Recipefinder::CLI
     instructions
     until (user_input = gets.strip) == "exit"
       input_instructions(user_input)
+      choose_recipe(user_input)
     end
   end
 
@@ -27,7 +28,7 @@ class Recipefinder::CLI
   def input_instructions(user_input)
     case user_input
     when "1"
-      puts "top 10 appetizer recipes"
+      appetizer_recipes
     when "2"
       puts "top 10 main dishes recipes"
     when "3"
@@ -49,18 +50,35 @@ class Recipefinder::CLI
     @num_of_choices = Recipe.all.count
   end
 
-  def lunch_recipes
-
+  def main_recipes
+    main_scrap = Scrapper.new
+    main_scrap.site = websites[main_dishes]
+    counter = 1
+    Recipe.all.each do |recipe|
+      puts "#{counter}." + " #{r.title}"
+      puts "By #{r.chef}"
+      counter += 1
+    end
+    @num_of_choices = Recipe.all.count
   end
 
-  def dinner_recipes
-
+  def desserts_recipes
+    desserts_scrap = Scrapper.new
+    desserts_scrap.site = websites[main_dishes]
+    counter = 1
+    Recipe.all.each do |recipe|
+      puts "#{counter}." + " #{r.title}"
+      puts "By #{r.chef}"
+      counter += 1
+    end
+    @num_of_choices = Recipe.all.count
   end
 
   def choose_recipe(user_input)
     puts "Choose the number of the recipe you'd like to cook:"
+    user_input = gets.strip
     if user_input <= self.num_of_choices && user_input > 0
-      recipe_find
+      recipe_find(user_input)
     else
       puts "Error: Please enter a number between 1 and #{self.num_of_choices}."
       puts "If your ready to start cookin' and want to exit, type 'exit'."
@@ -69,5 +87,7 @@ class Recipefinder::CLI
 
   def recipe_find(user_input)
     r = Recipe.all[user_input.to_i]
+    puts r.ingredients
+    puts r.directions
   end
 end
